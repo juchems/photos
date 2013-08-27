38473292e268aff260b6b63fb4a82bd361b62f47
juchems <juchems@gmail.com>
2013-08-26 22:51:54 -0500

    more junk files to start with

---
+++ phpMM/scripts/sayHello.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<link href="../css/phpMM.css" rel="stylesheet" type="text/css" />
+	</head>
+	
+	<body>
+		<div id="header"><h1>PHP & MySQL: The missing damn manual.</h1><div>
+		<div id="example">Example 2-1</div>
+		<div id="content">
+			<h1>Hello <?php echo $_REQUEST['name']; ?></h1>
+			<p> Turtles are delicious.</p>
+		</div>
+		<div id="footer"></div>
+	</body>
+</html>
